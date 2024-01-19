550e3e1220e42cc850f6eff5e5dbb9467e490539
Louis-Ladd <lewisharshman1@gmail.com>
2024-01-19 08:51:49 -0700

    Fixed pin current direction

--- keypadPromicro.ino
+++ keypadPromicro.ino
@@ -1,63 +1,68 @@
 #include <Keyboard.h>
 
 const byte ROWS = 3;
-const byte COLUMNS = 3;
+const byte COLUMNS = 7;
+const bool DEBUG = true;
+const int MAX_KEYS_PRESSED = 21;
 
 
-byte cols[COLUMNS] = { 5, 6, 7 };
-byte rows[ROWS] = { 2, 3, 4 };
+bool KEYS_PRESSED[MAX_KEYS_PRESSED] = { 0 };
 
-String keys[COLUMNS][ROWS] = {
-  { "3", "6", "9" },
-  { "2", "5", "8" },
-  { "1", "4", "7" }
+//Must measure current FROM ROWS to COLUMNS due to the diode configuation.
+byte cols[COLUMNS] = { 2, 3, 4, 5, 6, 7, 8 };
+byte rows[ROWS] = { 9, 10, 16 };
+
+
+
+byte keys[ROWS][COLUMNS] = {
+  { 1, 2, 3, 4, 5, 6, 7 },
+  { 8, 9, 10, 11, 12, 13, 14 },
+  { 15, 16, 17, 18, 19, 20, 21 }
 };
 
-void setupKeypad() {
-  for (byte r = 0; r < COLUMNS; r++) {
-    pinMode(cols[r], OUTPUT);
-    digitalWrite(cols[r], HIGH);
+void setupKeyMatrix() {
+  for (byte c = 0; c < COLUMNS; c++) {
+    pinMode(cols[c], INPUT_PULLUP);
   }
-  for (byte c = 0; c < ROWS; c++) {
-    pinMode(rows[c], INPUT_PULLUP);
+  for (byte r = 0; r < ROWS; r++) {
+    pinMode(rows[r], OUTPUT);
+    digitalWrite(rows[r], HIGH);
   }
 }
 
-String getKey() {
-  for (byte r = 0; r < COLUMNS; r++) {
-    digitalWrite(cols[r], LOW);
-    for (byte c = 0; c < ROWS; c++) {
-      if (digitalRead(rows[c]) == LOW) {
-        delay(10);
-        if (digitalRead(rows[c]) == LOW) {
-          digitalWrite(cols[r], HIGH);
-          Serial.println(keys[r][c].c_str());
-          return keys[r][c];
-        }
+void scanKeyMatrix() {
+  for (int r = 0; r < ROWS; r++)
+  {
+    for (int c = 0; c < COLUMNS; c++)
+    {
+      digitalWrite(rows[r], LOW);
+
+      if (digitalRead(cols[c]) == 0){
+        KEYS_PRESSED[keys[r][c]-1] = true;
       }
-    }
-    digitalWrite(cols[r], HIGH);
-  }
-  return "";
-}
+      else{
+        KEYS_PRESSED[keys[r][c]-1] = false;
+      }
 
+      digitalWrite(rows[r], HIGH);
+    }
+  }
+  if (DEBUG)
+    delay(150); //Debugging purposes
+}
 
 
 // Executed once before loop starts looping
 void setup() {
   Serial.begin(115200);
   Keyboard.begin();
-  setupKeypad();
+  setupKeyMatrix();
 }
 
 // Loop is executed repeatedly
 void loop() {
-  String currentKey = getKey();
-
-  Serial.println(currentKey);
-  Keyboard.print(currentKey);
-
-  while (currentKey != "") {
-    currentKey = getKey();
-  }
+  scanKeyMatrix();
+  for (int i = 0; i < MAX_KEYS_PRESSED; i++)
+    Serial.print(KEYS_PRESSED[i] ? "true " : "false ");
+  Serial.println();
 }
